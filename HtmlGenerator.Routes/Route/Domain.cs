109431d061afb6309879eb6b20c6b38e9830f42f
razumovskiy <kolosovp94@gmail.com>
2020-09-15 12:07:13 +0300

    gitpage styles supplied

--- HtmlGenerator.Routes/Route/Domain.cs
+++ HtmlGenerator.Routes/Route/Domain.cs
@@ -2,6 +2,6 @@
 {
     public static class Domain
     {
-        public const string MainDomain = "C:/Users/razumovskiy/RiderProjects/HtmlGenerator/RootFolder/";
+        public const string MainDomain = "https://kolosovpetro.github.io//HtmlGenerator/RootFolder/";
     }
 }

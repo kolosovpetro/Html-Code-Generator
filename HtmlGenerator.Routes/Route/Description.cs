072366d36578d0c6e50b834730c0b71dd324b82a
razumovskiy <kolosovp94@gmail.com>
2020-09-16 22:28:43 +0300

    algorithm descriptions added

--- HtmlGenerator.Routes/Route/Description.cs
+++ HtmlGenerator.Routes/Route/Description.cs
@@ -5,12 +5,15 @@
         public static class ClassicalAlgorithms
         {
             public const string Root = "Classical Algorithms";
-            public const string SieveOfEratosthenes = "Sieve Of Eratosthenes";
-            public const string EuclideanGcd = "Euclidean Gcd";
-            public const string MaximumSubarraySum = "Maximum Subarray Sum";
+            public const string SieveOfEratosthenes = "Generates primes up to given maximum.";
+            public const string EuclideanGcd = "Finds GCD of two integers m and n.";
+
+            public const string MaximumSubarraySum =
+                "Finds maximum n-length subarray sum of given collection. Divide and Conquer.";
+
             public const string ClosestPairProblem = "Closest Pair Problem";
-            public const string ShuntingYard = "Shunting-Yard Algorithhm";
-            public const string KadaneAlgorithm = "Kadane Algorithm";
+            public const string ShuntingYard = "Converts infix expression to postfix. Input: n-size infix string.";
+            public const string KadaneAlgorithm = "Brutforce solution to the maximum sub-array problem.";
         }
 
         public static class DataStructures
@@ -21,41 +24,50 @@
         public static class HeapAlgorithms
         {
             public const string RootTitle = "Heap Algorithms";
-            public const string HeapPush = "Heap Push";
-            public const string HeapPop = "Heap Pop";
-            public const string HeapCalculateUp = "Heap Calculate Up";
-            public const string HeapCalculateDown = "Heap Calculate Down";
-            public const string FloydAlgorithm = "Floyd Heapify";
-            public const string HeapSort = "Heap Sort";
+            public const string HeapPush = "Pushes new item to the heap.";
+            public const string HeapPop = "Pops top element from the heap. Element is removed from the heap.";
+            public const string HeapCalculateUp = "Re-heapifies the n-size heap from up.";
+            public const string HeapCalculateDown = "Re-heapifies the n-size heap from down.";
+            public const string FloydAlgorithm = "Re-heapifies the n-size heap in O(n) time.";
+            public const string HeapSort = "Advanced sort alggorithm. Complexity: O(nlogn).";
         }
 
         public static class BtAlgorithms
         {
             public const string RootTitle = "Binary Tree Algorithms";
-            public const string PreOrderRecursive = "Pre-Order Traversal Recursive";
-            public const string InOrderRecursive = "In-Order Traversal Recursive";
-            public const string OutOrderRecursive = "Out-Order Traversal Recursive";
-            public const string PostOrderRecursive = "Post-Order Traversal Recursive";
-            public const string PreOrderIterative = "Pre-Order Traversal Iterative";
-            public const string InOrderIterative = "In-Order Traversal Iterative";
-            public const string OutOrderIterative = "Out-Order Traversal Iterative";
-            public const string PostOrderIterative = "Post-Order Traversal Iterative";
-            public const string BreadthFirstIterative = "Breadth-First Traversal Iterative";
-            public const string Height = "Height";
-            public const string Depth = "Depth";
+
+            public const string PreOrderRecursive =
+                "Process node N, then process Left subtree, then process Right subtree.";
+
+            public const string InOrderRecursive = "Process Left subtree first, then node N, then Right subtree.";
+            public const string OutOrderRecursive = "Process Right subtree, then node N, then Left subtree.";
+            public const string PostOrderRecursive = "Process Left subtree, then Right subtree, then node N.";
+
+            public const string PreOrderIterative =
+                "Process node N, then process Left subtree, then process Right subtree.";
+
+            public const string InOrderIterative = "Process Left subtree first, then node N, then Right subtree.";
+            public const string OutOrderIterative = "Process Right subtree, then node N, then Left subtree.";
+            public const string PostOrderIterative = "Process Left subtree, then Right subtree, then node N.";
+
+            public const string BreadthFirstIterative =
+                "Visit all nodes on the current level before processing next node.";
+
+            public const string Height = "Returns the height of the tree.";
+            public const string Depth = "Returns depth of current node.";
         }
 
         public static class BstAlgorithms
         {
             public const string Root = "Binary Search Tree Algorithms";
-            public const string Insert = "BST Insert";
-            public const string Search = "BST Search";
-            public const string Delete = "BST Delete";
-            public const string Max = "BST Max";
-            public const string Min = "BST Min";
-            public const string Successor = "BST Successor";
-            public const string Predecessor = "BST Predecessor";
-            public const string Transplant = "BST Transplant";
+            public const string Insert = "Inserts new node to BST and gives pointer to it.";
+            public const string Search = "Searches for particular node in BST.";
+            public const string Delete = "Deletes node from bst.";
+            public const string Max = "Returns node with max key.";
+            public const string Min = "Returns node with min key.";
+            public const string Successor = "Returns Sucessor of given node.";
+            public const string Predecessor = "Returns Predecessor of given node.";
+            public const string Transplant = "Auxiliary method used in BST Delete.";
         }
 
         public static class TreeAlgorithms
@@ -66,31 +78,40 @@
         public static class GraphAlgorithms
         {
             public const string RootTitle = "Graph Algorithms";
-            public const string DijkstraAlgorithm = "Dijkstra Algorithm";
-            public const string BreadthFirst = "Breadth-First Graph Traversal";
-            public const string DepthFirstIterative = "Depth-First Graph Traversal Iterative";
-            public const string DepthFirstRecursive = "Depth-First Graph Traversal Recursive";
-            public const string KahnAlgorithm = "Kahn Algorithm (Topological Sort)";
+            public const string DijkstraAlgorithm = "Single shortest patch algorithm finder.";
+            public const string BreadthFirst = "Level order traversal of graph nodes.";
+            public const string DepthFirstIterative = "Recursively traversals all graph nodes.";
+            public const string DepthFirstRecursive = "Iterative traversal of all graph nodes.";
+            public const string KahnAlgorithm = "Presents graph as a linear sequence of nodes. Graph must be acyclic.";
         }
 
         public static class SortAlgorithms
         {
             public const string RootTitle = "Sort Algorithms";
-            public const string BubbleSort = "Bubble Sort";
-            public const string CocktailSort = "Cocktail Sort";
-            public const string InsertionSort = "Insertion Sort";
-            public const string SelectionSort = "Selection Sort";
-            public const string MergeSort = "Merge Sort";
-            public const string QuickSort = "Quick Sort";
+            public const string BubbleSort = "Simple Sort Algorithm. Sorts sequence of integers.";
+            public const string CocktailSort = "Simple Sort Algorithm. Sorts sequence of integers.";
+            public const string InsertionSort = "Simple Sort Algorithm. Sorts sequence of integers.";
+            public const string SelectionSort = "Simple Sort Algorithm. Sorts sequence of integers.";
+            public const string MergeSort = "Advanced Sort Algorithm. Sorts sequence of integers.";
+            public const string QuickSort = "Advanced Sort Algorithm. Sorts sequence of integers.";
         }
 
         public static class SearchAlgorithms
         {
-            public const string RootTitle = "Search Algorithms";
-            public const string SimpleLinearSearch = "Simple Linear Search";
-            public const string ImproveLinearSearch = "Improved Linear Search";
-            public const string ImproveLinearSearchWithSentinel = "Improved Linear Search With Sentinel";
-            public const string BinarySearch = "Binary Search";
+            public const string RootTitle =
+                "Searches for particular value in integer sequence. Returns true if found, otherwise false.";
+
+            public const string SimpleLinearSearch =
+                "Searches for particular value in integer sequence. Returns true if found, otherwise false.";
+
+            public const string ImproveLinearSearch =
+                "Searches for particular value in integer sequence. Returns true if found, otherwise false.";
+
+            public const string ImproveLinearSearchWithSentinel =
+                "Searches for particular value in integer sequence. Returns true if found, otherwise false.";
+
+            public const string BinarySearch =
+                "Searches for particular value in integer sequence. Returns true if found, otherwise false. Sequence must be sorted. Complexity O(logn).";
         }
     }
 }

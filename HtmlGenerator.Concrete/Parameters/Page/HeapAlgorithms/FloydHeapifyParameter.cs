5803495a25eeaa0041b176f2d00c83460143f1e3
razumovskiy <kolosovp94@gmail.com>
2020-09-18 22:25:38 +0300

    heap parameters added

---
+++ HtmlGenerator.Concrete/Parameters/Page/HeapAlgorithms/FloydHeapifyParameter.cs
@@ -0,0 +1,13 @@
+﻿using HtmlGenerator.Abstractions.Interfaces;
+using HtmlGenerator.Routes.Route;
+
+namespace HtmlGenerator.Concrete.Parameters.Page.HeapAlgorithms
+{
+    public class FloydHeapifyParameter : IPageParameter
+    {
+        public string DirectoryName => Path.HeapAlgorithms.FloydAlgorithm;
+        public string SubTitle => Title.HeapAlgorithms.FloydAlgorithm;
+        public string SnippetPath => Snippet.HeapAlgorithms.FloydAlgorithm;
+        public string Description => Routes.Route.Description.HeapAlgorithms.FloydAlgorithm;
+    }
+}
---
+++ HtmlGenerator.Concrete/Parameters/Page/HeapAlgorithms/HeapCalculateDownParameter.cs
@@ -0,0 +1,13 @@
+﻿using HtmlGenerator.Abstractions.Interfaces;
+using HtmlGenerator.Routes.Route;
+
+namespace HtmlGenerator.Concrete.Parameters.Page.HeapAlgorithms
+{
+    public class HeapCalculateDownParameter : IPageParameter
+    {
+        public string DirectoryName => Path.HeapAlgorithms.HeapCalculateDown;
+        public string SubTitle => Title.HeapAlgorithms.HeapCalculateDown;
+        public string SnippetPath => Snippet.HeapAlgorithms.HeapCalculateDown;
+        public string Description => Routes.Route.Description.HeapAlgorithms.HeapCalculateDown;
+    }
+}
